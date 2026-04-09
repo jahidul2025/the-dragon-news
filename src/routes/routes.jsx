a7e32e8e210bbb61cee9c0f5b0acaf7c4db8a8c6
jahidul islam <jahidul20673@gmail.com>
2026-04-09 21:43:31 +0600

    feat: initialize React project with Vite, Tailwind CSS, and React Router configuration

---
+++ src/routes/routes.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router";
+import HomeLayout from "../layouts/HomeLayout";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <HomeLayout></HomeLayout>
+    },
+    {
+        path: "/auth",
+        element: <h2>Welcome to the Authentication Page</h2>
+    },
+    {
+        path: "/news",
+        element: <h2>Welcome to the News Page</h2>
+    },
+    {
+        path: "/*",
+        element: <h2>error404: Page Not Found</h2>
+    }
+
+]);
+
+export default router;
